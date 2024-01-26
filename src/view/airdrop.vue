703888a19d21f3326162c9c32500c892b6efcc69
knightlong <921490600@qq.com>
2024-01-26 16:31:10 +0800

    first

--- src/view/airdrop.vue
+++ src/view/airdrop.vue
@@ -1629,6 +1629,7 @@ let tasklis = () => {
       appId: "xbot",
     })
     .then((res) => {
+      console.log("xbot", res.data.data);
       tasklist.value = res.data.data.userTaskList;
       tasklist.value.map((res) => {
         if (res.taskName == "register" && res.completed == true) {
