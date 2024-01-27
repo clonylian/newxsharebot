ae82609d608b412b0862fe1131c60941970d7510
knightlong <921490600@qq.com>
2024-01-27 10:59:47 +0800

    first

--- src/view/airdrop.vue
+++ src/view/airdrop.vue
@@ -1597,6 +1597,7 @@ onMounted(() => {
     firstlogin();
   }
   if (localStorage.getItem("Twname")) {
+    Twname.value = localStorage.getItem("Twname");
     alllogin.value = "1";
     xlogin.value = "0";
     xloginzt = "CONNECTED";
