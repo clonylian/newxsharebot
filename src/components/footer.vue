a5fd872a7539445d25c21cd64cf92aba9ba198f2
knightlong <921490600@qq.com>
2024-02-02 19:54:41 +0800

    first

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="ynewfoot">
     <div class="ynfootbox flex jus">
-      <span>@2023 NAVl. All rights reserved.</span>
-      <p class="ynfootend flex">
-        <router-link to="/terms" target="_blank">Terms of Service</router-link>
-        <router-link to="/eigenlayer" target="_blank"
-          >Privacy Policy</router-link
-        >
-      </p>
+      <div class="ynfootbl flex">
+        <span>@2023 NAVl. All rights reserved.</span>
+        <div class="ynfootblrou flex">
+          <router-link to="/terms" target="_blank"
+            >Terms of Service</router-link
+          >
+          <router-link to="/eigenlayer" target="_blank"
+            >Privacy Policy</router-link
+          >
+        </div>
+      </div>
+
+      <p class="ynfootend flex">Audited by <a href="">otterSec, Movebit</a></p>
     </div>
   </div>
 </template>
@@ -32,12 +38,40 @@ export default {};
   border-top: 1px solid rgb(179 187 202);
   padding: 1rem 0;
 }
-.ynfootbox > span {
+.ynfootbl {
+  column-gap: 1rem;
+  align-items: center;
+}
+.ynfootbl > span {
   font-family: "Inter";
+  font-size: 1rem;
+  line-height: 1.5rem;
   color: #6a7986;
 }
-.ynfootend {
+.ynfootblrou {
   column-gap: 1rem;
+}
+.ynfootblrou > a {
+  font-family: "Inter";
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: #6a7986;
+  transition: all 0.4s;
+}
+.ynfootblrou > a:hover {
+  color: white;
+}
+.ymainhy .ynfootblrou > a:hover {
+  color: #000;
+}
+.ynfootend {
+  font-family: "Inter";
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: #6a7986;
+  align-items: center;
+
+  column-gap: 0.5rem;
 }
 .ymainhy .ynfootbox > span {
   color: black;
@@ -56,6 +90,10 @@ export default {};
     width: 95%;
     display: block;
     text-align: center;
+  }
+  .ynfootbl {
+    justify-content: center;
+    flex-direction: column;
   }
   .ynewfoot {
     padding-top: 2rem;
